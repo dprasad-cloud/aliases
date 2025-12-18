2c769d34f6f0924a907cb2974d80865c42723d1a
dprasad <dprasad@extremenetworks.com>
2025-12-17 19:08:46 -0500

    work stuff

--- findsvc.sh
+++ findsvc.sh
@@ -26,5 +26,5 @@ SEARCH_TERM_CLEANED=$(echo "$SEARCH_TERM" | \
 kubectl get svc -A | grep -iE "$SEARCH_TERM_CLEANED"
 
 # Execute the function with the script's arguments
-echo -e "\n Executed:\n kubectl get svc -A | grep -iE \"$SEARCH_TERM_CLEANED\""
+echo -e "\n \nexecuted:\n\t kubectl get svc -A | grep -iE \"$SEARCH_TERM_CLEANED\""
 echo -e "\n"
