1c211968fd3e602d5932a7505ad0976cb21653b9
dprasad <dprasad@extremenetworks.com>
2026-04-13 19:17:48 -0400

    work stuff

--- check_usage.sh
+++ check_usage.sh
@@ -27,14 +27,16 @@ function how_long_ago(ts) {
 # 1. Map top data
 NR==FNR {u_cpu[$1$2]=$3; u_mem[$1$2]=$4; next}
 
-# 2. Process and Force Trim
+# 2. Process and Force 45-char Trim
 ($1$2) in u_cpu {
    uc=to_m(u_cpu[$1$2]); lc=to_m($3);
    um=to_mi(u_mem[$1$2]); rm=to_mi($4); lm=to_mi($5);
 
    p_name = $2;
-   if (length(p_name) > 60) {
-       display_pod = substr(p_name, 1, 57)"..."
+   gsub(/[[:space:]]/, "", p_name); # Clean hidden spaces
+
+   if (length(p_name) > 45) {
+       display_pod = substr(p_name, 1, 42)"..."
    } else {
        display_pod = p_name
    }
@@ -52,7 +54,8 @@ NR==FNR {u_cpu[$1$2]=$3; u_mem[$1$2]=$4; next}
    rp=(rm>0)?(um/rm)*100:0;
    lp=(lm>0)?(um/lm)*100:0;
 
-   printf "%-10s | %-60.60s | C: %-5s/%-5s (%3d%%) | M: %-7s | R: %-5s (%3d%%) | L: %-5s (%3d%%) | %s\n",
+   # %-45.45s handles the strict display limit
+   printf "%-10s | %-45.45s | C: %-5s/%-5s (%3d%%) | M: %-7s | R: %-5s (%3d%%) | L: %-5s (%3d%%) | %s\n",
           $1, display_pod, u_cpu[$1$2], $3, cp, u_mem[$1$2], $4, rp, $5, lp, restart_info
 }' <(kubectl top pods -A --no-headers | grep "$FILTER" | awk '{print $1"\t"$2"\t"$3"\t"$4}') \
    <(kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.spec.containers[0].resources.limits.cpu}{"\t"}{.spec.containers[0].resources.requests.memory}{"\t"}{.spec.containers[0].resources.limits.memory}{"\t"}{.status.containerStatuses[0].restartCount}{"\t"}{.status.containerStatuses[0].lastState.terminated.finishedAt}{"\n"}{end}' | grep "$FILTER") \
