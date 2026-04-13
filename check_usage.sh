49b5bacba5d1f6cd238750bf86ff04627932d95a
dprasad <dprasad@extremenetworks.com>
2026-04-13 19:24:33 -0400

    work stuff

--- check_usage.sh
+++ check_usage.sh
@@ -1,6 +1,13 @@
 FILTER=$1
 NOW=$(date +%s)
 
+# Determine if we should filter or show everything
+if [[ "$FILTER" == "all" || "$FILTER" == "ALL" || -z "$FILTER" ]]; then
+    GREP_CMD="cat"
+else
+    GREP_CMD="grep $FILTER"
+fi
+
 awk -v now="$NOW" 'BEGIN {FS="\t"; OFS=" | "}
 function to_mi(val) {
    if (val ~ /[Gg]i?/) { sub(/[Gg]i?/, "", val); return val * 1024 }
@@ -33,7 +40,7 @@ NR==FNR {u_cpu[$1$2]=$3; u_mem[$1$2]=$4; next}
    um=to_mi(u_mem[$1$2]); rm=to_mi($4); lm=to_mi($5);
 
    p_name = $2;
-   gsub(/[[:space:]]/, "", p_name); # Clean hidden spaces
+   gsub(/[[:space:]]/, "", p_name);
 
    if (length(p_name) > 45) {
        display_pod = substr(p_name, 1, 42)"..."
@@ -54,9 +61,8 @@ NR==FNR {u_cpu[$1$2]=$3; u_mem[$1$2]=$4; next}
    rp=(rm>0)?(um/rm)*100:0;
    lp=(lm>0)?(um/lm)*100:0;
 
-   # %-45.45s handles the strict display limit
    printf "%-10s | %-45.45s | C: %-5s/%-5s (%3d%%) | M: %-7s | R: %-5s (%3d%%) | L: %-5s (%3d%%) | %s\n",
           $1, display_pod, u_cpu[$1$2], $3, cp, u_mem[$1$2], $4, rp, $5, lp, restart_info
-}' <(kubectl top pods -A --no-headers | grep "$FILTER" | awk '{print $1"\t"$2"\t"$3"\t"$4}') \
-   <(kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.spec.containers[0].resources.limits.cpu}{"\t"}{.spec.containers[0].resources.requests.memory}{"\t"}{.spec.containers[0].resources.limits.memory}{"\t"}{.status.containerStatuses[0].restartCount}{"\t"}{.status.containerStatuses[0].lastState.terminated.finishedAt}{"\n"}{end}' | grep "$FILTER") \
+}' <(kubectl top pods -A --no-headers | $GREP_CMD | awk '{print $1"\t"$2"\t"$3"\t"$4}') \
+   <(kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.spec.containers[0].resources.limits.cpu}{"\t"}{.spec.containers[0].resources.requests.memory}{"\t"}{.spec.containers[0].resources.limits.memory}{"\t"}{.status.containerStatuses[0].restartCount}{"\t"}{.status.containerStatuses[0].lastState.terminated.finishedAt}{"\n"}{end}' | $GREP_CMD) \
 | column -t -s '|'
