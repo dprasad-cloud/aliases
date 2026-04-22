7c6b5b47b45c17d8cad9edb1fa5728313d6a15ce
dprasad <dprasad@extremenetworks.com>
2026-04-21 23:17:15 -0400

    work stuff

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 1. IMPORTANT: Enable alias expansion inside this script
+shopt -s expand_aliases
+source "/root/aliases-main/cloudv2.txt"
+
 disk_usage() {
     # If an argument is provided, execute findpod and pipe it back
     if [ -n "$1" ]; then
