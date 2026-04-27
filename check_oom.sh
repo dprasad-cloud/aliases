44a82d069d37c8e7b03153799b90a3a3594d3049
dprasad <dprasad@extremenetworks.com>
2026-04-26 23:35:39 -0400

    work stuff

--- check_oom.sh
+++ check_oom.sh
@@ -3,6 +3,6 @@ kubectl get pods -A -o json \
   select(.lastState.terminated.reason == "OOMKilled" or .lastState.terminated.exitCode == 137) |
   [$ns, $pod, (.restartCount|tostring), (.lastState.terminated.finishedAt // "-")] | @tsv' \
 | awk -F'\t' 'BEGIN{OFS="\t"} {key=$1"\t"$2; c[key]++; r[key]=$3; t[key]=$4} END{for(k in c){print k, c[k], r[k], t[k]}}' \
-| sort -t$'\t' -k5,5nr -k4,4nr \
+| sort -t$'\t' -k5,5r \
 | { printf "NAMESPACE\tPOD\tOOM_MATCH_COUNT\tRESTART_COUNT\tLAST_OOM_FINISHED_AT\n"; cat; } \
 | column -t -s$'\t'
