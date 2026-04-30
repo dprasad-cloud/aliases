2d4b0088f7daf89281e1d6443bd6026b7950606b
dprasad <dprasad@extremenetworks.com>
2026-04-30 14:08:52 -0400

    work stuff

--- check_usage.sh
+++ check_usage.sh
@@ -52,16 +52,13 @@ NR==FNR {
    rc_val = to_m($3); lc_val = to_m($4);
    mr_val = to_mi($5); ml_val = to_mi($6);
 
-   # Process comma-separated restarts and timestamps
    split($7, restarts_arr, ",");
    split($8, times_arr, ",");
 
    total_restarts = 0;
    latest_ts = "0";
 
-   for (i in restarts_arr) {
-       total_restarts += restarts_arr[i];
-   }
+   for (i in restarts_arr) { total_restarts += restarts_arr[i] }
    for (j in times_arr) {
        if (times_arr[j] != "<nil>" && times_arr[j] != "0" && times_arr[j] > latest_ts) {
            latest_ts = times_arr[j];
@@ -78,9 +75,13 @@ NR==FNR {
        restart_info = "-";
    }
 
-   printf "%-12s | %-40s | C: %-5s %-10s %-15s | M: %-7s | %-12s %-15s | %s\n",
-          $1, display_pod, u_cpu[$1$2], $3"/"$4, sprintf("(%3d%% / %3d%%)", (rc_val>0?uc/rc_val*100:0), (lc_val>0?uc/lc_val*100:0)),
-          u_mem[$1$2], $5"/"$6, sprintf("(%3d%% / %3d%%)", (mr_val>0?um/mr_val*100:0), (ml_val>0?um/ml_val*100:0)), restart_info
+   # Calculate Mem % for sorting
+   mp_lim = (ml_val > 0) ? int((um / ml_val) * 100) : 0;
+
+   # We print mp_lim as the first field so sort -rn works, then cut it out
+   printf "%05d | %-12s | %-40s | C: %-5s %-10s %-15s | M: %-7s | %-12s %-15s | %s\n",
+          mp_lim, $1, display_pod, u_cpu[$1$2], $3"/"$4, sprintf("(%3d%% / %3d%%)", (rc_val>0?uc/rc_val*100:0), (lc_val>0?uc/lc_val*100:0)),
+          u_mem[$1$2], $5"/"$6, sprintf("(%3d%% / %3d%%)", (mr_val>0?um/mr_val*100:0), mp_lim "%"), restart_info
 }' <(kubectl top pods -A --no-headers) \
    <(kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.namespace}{" "}{.metadata.name}{" "}{.spec.containers[0].resources.requests.cpu}{" "}{.spec.containers[0].resources.limits.cpu}{" "}{.spec.containers[0].resources.requests.memory}{" "}{.spec.containers[0].resources.limits.memory}{" "}{range .status.containerStatuses[*]}{.restartCount}{","}{end}{" "}{range .status.containerStatuses[*]}{.lastState.terminated.finishedAt}{","}{end}{"\n"}{end}') \
-| sort -k 1 | column -t -s '|'
+| sort -rn | cut -d '|' -f 2- | column -t -s '|'
