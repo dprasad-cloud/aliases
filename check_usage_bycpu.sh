679f44db414a143bce8edab16fc5073dc0f48d59
dprasad <dprasad@extremenetworks.com>
2026-05-08 16:29:41 -0400

    work stuff

--- check_usage_bycpu.sh
+++ check_usage_bycpu.sh
@@ -58,14 +58,11 @@ NR==FNR {
    mp_req = (mr_val > 0) ? (um / mr_val) * 100 : 0;
    mp_lim = (ml_val > 0) ? (um / ml_val) * 100 : 0;
 
-   total_restarts = $7;
-   latest_ts = $8;
-
    p_name = $2;
    display_pod = (length(p_name) > 27) ? substr(p_name, 1, 20) ".*" substr(p_name, length(p_name) - 4) : p_name;
 
-   time_ago = how_long_ago(latest_ts);
-   restart_info = (total_restarts > 0) ? ((time_ago != "") ? time_ago " (" total_restarts ")" : "(" total_restarts ")") : "-";
+   time_ago = how_long_ago($8);
+   restart_info = ($7 > 0) ? ((time_ago != "") ? time_ago " (" $7 ")" : "(" $7 ")") : "-";
 
    cpu_rl = $3 "/" $4;
    mem_rl = $5 "/" $6;
@@ -73,20 +70,20 @@ NR==FNR {
    cpu_perc = sprintf("(%3.1f%% / %3.1f%%)", cp_req, cp_lim);
    mem_perc = sprintf("(%3.1f%% / %3.1f%%)", mp_req, mp_lim);
 
-   # Sort key is now cp_req (Requested CPU %)
+   # Primary Sort: cp_req (Requested CPU %)
    printf "%10.2f|%-12s|%-27.27s|C: %-6s %-12s %-18s|M: %-7s %-12s %-18s|%s\n",
           cp_req, $1, display_pod, u_cpu[$1$2], cpu_rl, cpu_perc, u_mem[$1$2], mem_rl, mem_perc, restart_info
 }' <(kubectl top pods -A --no-headers) \
    <(kubectl get pods -A -o json | jq -r '.items[] | select(.status.phase == "Running") |
-      def to_ms: if . == null then 0 elif (tostring | endswith("m")) then (tostring[:-1] | tonumber) else (tostring | tonumber * 1000) end;
-      def to_mib: if . == null then 0 elif (tostring | endswith("Ki")) then (tostring[:-2] | tonumber / 1024) elif (tostring | endswith("Mi")) then (tostring[:-2] | tonumber) elif (tostring | endswith("Gi")) then (tostring[:-2] | tonumber * 1024) else (tostring | tonumber / 1024 / 1024) end;
+      def to_ms: tostring | if endswith("m") then .[:-1] | tonumber elif contains(".") or (gsub("[^0-9.]"; "") | tonumber < 50) then (gsub("[^0-9.]"; "") | tonumber * 1000) else (gsub("[^0-9.]"; "") | tonumber) end;
+      def to_mib: tostring | if endswith("Ki") then .[:-2] | tonumber / 1024 elif endswith("Mi") then .[:-2] | tonumber elif endswith("Gi") then .[:-2] | tonumber * 1024 else (gsub("[^0-9.]"; "") | tonumber / 1024 / 1024) end;
       [
          .metadata.namespace,
          .metadata.name,
-         ([.spec.containers[].resources.requests.cpu // "0m"] | map(to_ms) | add | tostring + "m"),
-         ([.spec.containers[].resources.limits.cpu // "0m"] | map(to_ms) | add | tostring + "m"),
-         ([.spec.containers[].resources.requests.memory // "0Mi"] | map(to_mib) | add | tostring + "Mi"),
-         ([.spec.containers[].resources.limits.memory // "0Mi"] | map(to_mib) | add | tostring + "Mi"),
+         ([.spec.containers[].resources.requests.cpu // "0"] | map(to_ms) | add | tostring + "m"),
+         ([.spec.containers[].resources.limits.cpu // "0"] | map(to_ms) | add | tostring + "m"),
+         ([.spec.containers[].resources.requests.memory // "0"] | map(to_mib) | add | tostring + "Mi"),
+         ([.spec.containers[].resources.limits.memory // "0"] | map(to_mib) | add | tostring + "Mi"),
          ([.status.containerStatuses[].restartCount // 0] | add | tostring),
          ([.status.containerStatuses[].lastState.terminated.finishedAt // "0"] | sort | last | tostring)
       ] | @tsv') \
