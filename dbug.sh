f107adbc79b5b8500f1a95025d3975573f768586
dprasad <dprasad@extremenetworks.com>
2026-06-08 10:43:54 -0400

    work stuff

--- dbug.sh
+++ dbug.sh
@@ -60,36 +60,32 @@ NR==FNR {
    pod_part = $2;
    con_part = $3;
 
-   # Smart dynamic truncation ensuring overall string structure sits exactly under 33 characters
-   if (length(pod_part) + length(con_part) + 1 <= 33) {
-       display_name = pod_part "/" con_part
-   } else {
-       p_len = length(pod_part)
-       c_len = length(con_part)
+   # Forced formatting to always use the podname.*contname schema
+   p_len = length(pod_part)
+   c_len = length(con_part)
 
-       total_raw_len = p_len + c_len + 9
-       overflow = total_raw_len - 33
+   total_raw_len = p_len + c_len + 9
+   overflow = total_raw_len - 33
 
-       if (overflow > 0 && c_len > 10) {
-           c_reduction = (c_len - 10 >= overflow) ? overflow : (c_len - 10)
-           c_len = c_len - c_reduction
-           overflow = overflow - c_reduction
-       }
-
-       if (overflow > 0 && c_len > 5) {
-           c_reduction = (c_len - 5 >= overflow) ? overflow : (c_len - 5)
-           c_len = c_len - c_reduction
-       }
-
-       p_end = (p_len >= 5) ? substr(pod_part, p_len - 4) : pod_part
-       c_trim = substr(con_part, length(con_part) - c_len + 1)
-
-       start_len = 33 - 2 - 5 - 2 - length(c_trim)
-       if (start_len < 1) start_len = 1
-
-       p_start = substr(pod_part, 1, start_len)
-       display_name = p_start ".*" p_end ".*" c_trim
+   if (overflow > 0 && c_len > 10) {
+       c_reduction = (c_len - 10 >= overflow) ? overflow : (c_len - 10)
+       c_len = c_len - c_reduction
+       overflow = overflow - c_reduction
    }
+
+   if (overflow > 0 && c_len > 5) {
+       c_reduction = (c_len - 5 >= overflow) ? overflow : (c_len - 5)
+       c_len = c_len - c_reduction
+   }
+
+   p_end = (p_len >= 5) ? substr(pod_part, p_len - 4) : pod_part
+   c_trim = substr(con_part, length(con_part) - c_len + 1)
+
+   start_len = 33 - 2 - 5 - 2 - length(c_trim)
+   if (start_len < 1) start_len = 1
+
+   p_start = substr(pod_part, 1, start_len)
+   display_name = p_start ".*" p_end ".*" c_trim
 
    time_ago = how_long_ago($9);
    raw_restart = ($8 > 0) ? ((time_ago != "") ? time_ago "(" $8 ")" : "(" $8 ")") : "-";
